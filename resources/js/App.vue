f64b5174d0380cd5524ea5ba4b95e4d73068896a
anhnoy <136299027+anhnoy@users.noreply.github.com>
2024-02-26 16:12:26 +0700

    hoc vue ep1

---
+++ resources/js/App.vue
@@ -0,0 +1,26 @@
+<template lang="">
+    <div>
+        <h1>
+            hello vue app
+        </h1>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Minipos10App',
+    data() {
+        return {
+            
+        }
+    },
+    mounted() {
+        
+    },
+    methods: {
+
+    }
+}
+</script>
+<style lang="scss" scoped>
+    
+</style>
